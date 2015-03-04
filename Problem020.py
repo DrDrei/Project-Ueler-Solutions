f08c73325525423ed39aa492de31aa3fcd68f6cd
Andrei <drei3000@gmail.com>
2015-03-04 11:40:51 -0700

    Solved Problem 20

--- Problem020.py
+++ Problem020.py
@@ -1,33 +1,14 @@
-import sys
+import sys 
 import math
 
+n = 100
+product = 1
+for i in range(1,n+1):
+	product = product * i
 
+product = str(product)
+sum = 0
+for i in product:
+	sum += int(i)
 
-def feb(year):
-	if not (year % 4):
-		return 29
-	else:
-		return 28
-
-def months(month, year):
-	if month == 4 or month == 6 or month == 9 or month == 11:
-		return 30
-	elif month == 2:
-		return feb(year)
-	else:
-		return 31
-
-count = 0
-weekday = 0
-for year in range(1901,2000+1):
-	for month in range(1,13):
-		for day in range(1,months(month,year)+1):
-			weekday += 1
-			if weekday == 8:
-				weekday = 1
-			if 1 == day and 1 == weekday:
-				count += 1
-				print(count)
-			print(day,weekday,month,year)
-
-print(count)
+print(sum)
