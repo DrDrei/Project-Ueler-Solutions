296f9bc948431642bdced2a3d79e8ef35fd4c439
Drei <drei3000@gmail.com>
2016-04-07 20:20:43 -0600

    Finished Problem 25

--- Problem025.py
+++ Problem025.py
@@ -1,2 +1,20 @@
-import sys 
-import math
+import time
+
+def main():
+    fibList = list()
+    fibList.append(1)
+    fibList.append(1)
+    strLength = len(str(fibList[-1]))
+    startTime = time.time()
+    while strLength != 1000:
+        fibList.append(fib(fibList[-1], fibList[-2]))
+        strLength = len(str(fibList[-1]))
+    endTime = time.time()
+    print('Calculation took: ' + str(endTime-startTime) + 'sec')
+    print('Index: ' + str(len(fibList)))
+    
+def fib(one, two):
+    return one+two   
+
+if __name__ == "__main__":
+    main()
